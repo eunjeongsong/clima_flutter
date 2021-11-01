864b625121667e8c80dce29895d7f41fb88d0530
EUNJEONGSONG <hellogine12@gmail.com>
2021-11-01 23:27:51 +0900

    add open weather api and get weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:clima_flutter/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+const appId = "a49275fd8d9c962e9d3fd1b0bf4146aa";
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -16,47 +22,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    // Future<Position> getLocation() async {
-    // bool serviceEnabled;
-    // LocationPermission permission;
-    //
-    // serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    // if (!serviceEnabled) {
-    //   // Location services are not enabled don't continue
-    //   // accessing the position and request users of the
-    //   // App to enable the location services.
-    //   return Future.error('Location services are disabled.');
-    // }
-    //
-    // permission = await Geolocator.checkPermission();
-    // if (permission == LocationPermission.denied) {
-    //   permission = await Geolocator.requestPermission();
-    //   if (permission == LocationPermission.denied) {
-    //     // Permissions are denied, next time you could try
-    //     // requesting permissions again (this is also where
-    //     // Android's shouldShowRequestPermissionRationale
-    //     // returned true. According to Android guidelines
-    //     // your App should show an explanatory UI now.
-    //     return Future.error('Location permissions are denied');
-    //   }
-    // }
-    //
-    // if (permission == LocationPermission.deniedForever) {
-    //   // Permissions are denied forever, handle appropriately.
-    //   return Future.error(
-    //       'Location permissions are permanently denied, we cannot request permissions.');
-    // }
-    //
-    // // When we reach here, permissions are granted and we can
-    // // continue accessing the position of the device.
-    // return await Geolocator.getCurrentPosition();
     try {
       Location location = Location();
       await location.getCurrentLocation();
-      print(location.latitude);
-      print(location.longitude);
+      double latitude = location.latitude;
+      double longitude = location.longitude;
+      getData(latitude, longitude);
     } catch (e) {
       print(e);
+    }
+  }
+
+  void getData(latitude, longitude) async {
+    Uri url = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$appId');
+    Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      dynamic decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+    } else {
+      print(response.statusCode);
     }
   }
 
